3aef2b42f4f8c5a2ab81014693a2b7efd315a45a
Victor Martin <vmleon@gmail.com>
2023-07-03 17:13:55 +0200

    fix new env home provider for IAM

---
+++ dev/terraform/data.tf
@@ -0,0 +1,14 @@
+data "oci_identity_tenancy" "tenant_details" {
+  tenancy_id = var.tenancy_ocid
+
+  provider = oci
+}
+
+data "oci_identity_regions" "home_region" {
+  filter {
+    name   = "key"
+    values = [data.oci_identity_tenancy.tenant_details.home_region_key]
+  }
+
+  provider = oci
+}
